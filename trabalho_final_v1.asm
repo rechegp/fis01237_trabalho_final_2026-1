6d5dd10ba36c26cfa5e58ea42f6b17cc1e8bdc03
pedro <contact@rechegp.com>
2026-06-26 18:01:13 -0300

    adicionei botões REPETE e MAIS LONGO, ver changelog.txt

--- trabalho_final_v1.asm
+++ trabalho_final_v1.asm
@@ -14,6 +14,12 @@
 jmp init
 ; botão por LED - cada LED teria seu próprio botão
 
+.org 0x0002
+jmp external_interrupt_0 ; interruṕção externa 0 com botão - função "REPETE" do Genius original
+
+.org 0x0004
+jmp external_interrupt_1 ; interrupção externa 1 com botão - função "MAIS LONGO" do Genius original
+
 .org 0x0016
 jmp timer1_ocra_match ; Usando esta interrupção para ver se o tempo limite entre pressionamentos esgotou
 
@@ -32,7 +38,7 @@ init:
 
      SetStack RAMEND, R16
 
-     ; Configuração PORTC para LEDs e PORTD para botões
+     ; Configuração das portas digitais usadas para o jogo
 
 
      ldi R16, (1 << PINB3)
@@ -42,8 +48,11 @@ init:
      out PORTC, R16
      sbr R16, 0x0F ; half-byte inferior usado para LEDs na porta C.
      out DDRC, R16
-     com R16 ; half-byte superior usado para botões na porta D.
+     ldi R16, 0xFC ; half-byte superior usado para botões na porta D. Bits 2 e 3 da porta usados para botões (interrupção externa)
      out PORTD, R16
+
+     ; Configuração do ADC
+
      ldi R16, 0b00_110000 ; mantém ligados pinos 4 e 5 do multiplexador do ADC, mas desliga os outros para evitar conflitos com LEDs da porta C
      sts DIDR0, R16
      ldi R16, 0b11_1_0_0101 ; Usa referência de tensão do ADC em 1,1V; usa modo 8 ou10 bits (só lê ADCH ou ADCL); usa porta 5 do ADC para leitura do ruído
@@ -54,18 +63,30 @@ init:
      clr R16 ; desliga MUX em modo comparação; ADC em modo single conversion (pois autotrigger está desligado)
      sts ADCSRB, R16
 
+     ; Configuração das interrupções externas por botão
+
+     ldi R16, (1 << ISC11) | (1 << ISC01) ; configura interrupções para serem acionadas no falling edge
+     sts EICRA, R16
+
+     ; register EIMSK é configurado diretamente na SR menu_config
+
+
      ; Configuração endereçamento indireto com deslocamento
 
      reset_z_pointer ; chama a macro respectiva
 
      ; demais variáveis do programa
 
-     clr R03 ; R03 será atualizado com a referência do valor máximo de R02 (tamanho da sequência na memória)
-     mov R02, R03 ; R02 será usado como índice da sequência
+     clr R3 ; R03 será atualizado com a referência do valor máximo de R02 (tamanho da sequência na memória)
+     mov R2, R3 ; R02 será usado como índice da sequência
+     mov R4, R3 ; R04 será usado para definir tamanho da maior sequência (usado no botão "MAIS LONGO")
+     clr R5 ; variável booleana para definir se alguma interrupção externa (0 ou 1) está ativa
+     clr R6 ; variável que grava o intervalo de tempo de LED/buzzer ligado da tentativa recorde (usado no botão "MAIS LONGO")
+     clr R7 ; variável que grava o intervalo de tempo de LED/buzzer ligado da tentativa recorde (usado no botão "MAIS LONGO")
      clr R17 ; variável de dificuldade;  tempo limite do modo de jogo (pressionamento entre botões)
      clr R18 ; variável de dificuldade; tempo que o LED/buzzer fica ligado por etapa
      clr R19 ; variável de dificuldade; tempo que o LED fica desligado por etapa
-     clr R20 ; variável que grava o valor do pressionamento de botão
+     clr R20 ; variável que grava o valor do pressionamento de botão e também do valor lido da memória
      clr R21 ; variável de dificuldade; tempo entre rodadas
      clr R22 ; variável que recebe o valor "bruto" da leitura do ADC
      clr R23 ; variável que recebe codificação "one-hot" de acordo com os 2 LSBs do ADC
@@ -85,6 +106,7 @@ init:
 
 menu_config:
 
+            clr R2       ; garanto que mesmo quando direcionado à SR de gravar recorde, o índice começa em 0
             ldi R28, 0x01 ; reinicia animação dos LEDs do menu inicial
             dec R29       ; reinicia seletor de modo de jogo
             ldi R16, 0xA0 ; valor definido do ciclo de acendimento para cada LED
@@ -97,6 +119,9 @@ menu_config:
             sts TCNT1H, R27
             ldi R16, (1 << OCIE1B)
             sts TIMSK1, R16 ; liga a interrupção do timer 1 compare match OCRB
+
+            ldi R16, 0b000000_11 ; habilita ambas as interrupções externas dos botões REPETE e MAIS LONGO
+            out EIMSK, R16
 
             reset_z_pointer ; reinicia registrador apontador
 
@@ -167,11 +192,15 @@ gamemode_selected:
 
                   inc R29              ; indica que um modo de jogo está em vigor
                   sts TIMSK1, R27      ; desliga a interrupção do OCRB match
+                  out EIMSK, R27       ; desliga interrupções externas dos botões
                   out PORTC, R27       ; desliga LEDs do menu inicial
 
-                  ldi R16, 0x50        ; tempo para começo da partida
-
+                  ldi R16, 0x40        ; tempo para começo da partida
+                  clr R2
+                  clr R3
                   timer_next_round R16
+                  sts TCNT1L, R27 ; limpa timer 1 para começar zerado
+                  sts TCNT1H, R27
 
                   sbrc R20, 0
                   rjmp game1_start
@@ -239,8 +268,8 @@ read_and_load_random_val:    ; Lógica de enviar valor aleatório do ADC para o 
 read_sequence_from_sram: ; Lógica de ler valores da sequência em ordem crescente
 
 
-                         cpse R02, R03
-                         cpse R02, R02  ; truque para pular linha garantidamente
+                         cpse R2, R3
+                         cpse R2, R2  ; truque para pular linha garantidamente
                          rjmp reset_sequence_index ; vai para próxima etapa de transição
                          ld R20, Z+
                          flash_and_beep R20,R18,R19 ; macro para piscar LEDs e fazer o beep de acordo com a LED acesa
@@ -249,8 +278,12 @@ read_sequence_from_sram: ; Lógica de ler valores da sequência em ordem crescen
 
 reset_sequence_index:
 
-                     clr R02 ; limpa  R02 para ler desde o início da sequência (não pode ser feitos em subrotina com loop)
+                     clr R2 ; limpa  R02 para ler desde o início da sequência (não pode ser feitos em subrotina com loop)
                      reset_z_pointer ; reseta apontador Z para fazer a verificação da sequência do jogador
+
+                     cpse R5,R27 ; se interrupção externa por botão estiver ativa, sair desta SR aqui
+                     ret
+
                      ldi R16, 0b00_0_00_101 ; configura prescaler do timer em 1024
                      sts TCCR1B, R16
                      ldi R16, 0b00_00_00_00 ; configura timer em modo normal
@@ -267,7 +300,7 @@ check_sequence:
 
                clr R24 ; reset do tempo
                sts TIMSK1, R27 ; desliga interrupção temporariamente
-               inc R02 ; lê o próximo índice da sequência. Só é possível chegar aqui quando o pressionamento de um botão é confirmado
+               inc R2 ; lê o próximo índice da sequência. Só é possível chegar aqui quando o pressionamento de um botão é confirmado
                ld R16, Z+
                cpse R16, R20
                rjmp game_over ; errou! Fim de jogo.
@@ -297,7 +330,7 @@ check_button_depressed:
 next_step:
           cpse R15, R29 ; se modo de jogo estiver carregado, voltar para ponto em que SR foi chamada
           ret
-          cpse R02, R03 ; verificar se a sequência acabou
+          cpse R2, R3 ; verificar se a sequência acabou
           rjmp read_buttons
           ldi R16, 0b1_1_1_0_1_111 ; mesma instrução da configuração, para iniciar conversão novamente
           sts ADCSRA, R16
@@ -311,16 +344,120 @@ game_over: ; fim de jogo, retornar ao menu inicial
 
            ; reinicia variáveis do jogo
 
-          clr R02
-          clr R03
+          clr R2
+          ;clr R03
           clr R24
 
           sts TIMSK1, R27 ; desliga interrupção quando o jogo acaba
           timer_game_over
           out PORTC, R27 ; desliga LEDs
 
+          cp R3, R4 ; verifica se a tentativa atual superou ou igualou o recorde
+          brsh new_record
+
           jmp menu_config
 
+new_record:
+           dec R3      ; exclui-se o valor mais recente da sequência (mantém-se apenas os que vieram antes do game over)
+           mov R4, R3 ; grava tamanho da sequência recorde em register dedicado
+           mov R6, R18 ; grava tempo de acionamento buzzer/LEDs em register dedicado
+           mov R7, R19 ; grava tempo de LED/buzzer desligado em register dedicado
+           reset_z_pointer ; reinicia registrador apontador
+           rcall save_record_sequence ; chama SR para gravar sequência recorde na SRAM
+           jmp menu_config
+
+save_record_sequence:
+
+              ld R20, Z+        ; lê dado gravado na memória do jogo anterior
+              std Z + 0x3F, R20 ; grava dado em endereço 63 casas acima - com certeza ninguém vai conseguir ir tão longe pra quebrar isso...
+              inc R2
+              cpse R2, R4     ; fim da iteração quando chegar no fim da sequência recorde
+              rjmp save_record_sequence
+              ret
+
+
+external_interrupt_0:
+
+                     debounce_filter 0xFF ; usa timer 0, compara com valor chamado
+                     sbic PIND, 2 ; vê se botão INT0 realmente foi pressionado
+                     reti ; se não foi, volta para onde estava
+
+                     ; só salva variáveis no SP se aperto do botão for reconhecido
+
+                     push R16
+                     in R16, SREG
+                     push R16
+                     lds R16, TCCR1B
+                     push R16
+                     push R2
+                     push R3
+
+                     cpse R3, R4
+                     cpse R3, R3 ; se não for, não muda nada
+                     inc R3 ; aumenta tamanho da sequência  se última sequência é de mesmo tamanho que a recorde
+
+                     inc R5
+
+                     call read_sequence_from_sram
+
+                     dec R5
+
+                     pop R3
+                     pop R2
+                     pop R16
+                     sts TCCR1B, R16
+                     pop R16
+                     out SREG, R16
+                     pop R16
+
+                     reti
+
+external_interrupt_1:
+
+
+
+debounce_filter 0xFF ; usa timer 0, compara com valor chamado
+                     sbic PIND, 3 ; vê se botão INT0 realmente foi pressionado
+                     reti ; se não foi, volta para onde estava
+
+                     ; só salva variáveis no SP se aperto do botão for reconhecido
+
+                     push R16
+                     in R16, SREG
+                     push R16
+                     lds R16, TCCR1B
+                     push R16
+                     push R2
+                     push R3
+                     push R18
+                     push R19
+
+                     inc R5
+
+                     ldi ZL, 0x40 ; dados da sequência recorde são gravados a partir de 0x0140
+                     ldi ZH, 0x01
+
+                     mov R3, R4  ; carrega tamanho da sequência recorde
+                     mov R18, R6 ; carrega tempo de LED/buzzer ligado da sequência recorde
+                     mov R19, R7 ; carrega tempo de LED/buzzer desligado da seq uência recorde
+
+                     call read_sequence_from_sram
+
+                     ; implementar aqui lógica para botão REPETE
+
+                     dec R5
+
+                     pop R19
+                     pop R18
+                     pop R3
+                     pop R2
+                     pop R16
+                     sts TCCR1B, R16
+                     pop R16
+                     out SREG, R16
+                     pop R16
+
+                     reti
 
 timer1_ocra_match:
 
@@ -377,5 +514,3 @@ adc_interrupt:
               reti
 
 .include "macros/debounce_wait.asm"
-
-
